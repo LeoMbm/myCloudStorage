978c844a5f470612f5a2b2865a732eb556f64bd0
LeoMbm <leonidas.j.mbm@gmail.com>
2022-08-22 19:26:16 +0200

    update

---
+++ config/database.ts
@@ -0,0 +1,36 @@
+/**
+ * Config source: https://git.io/JesV9
+ *
+ * Feel free to let us know via PR, if you find something broken in this config
+ * file.
+ */
+
+import Env from '@ioc:Adonis/Core/Env'
+import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+
+const databaseConfig: DatabaseConfig = {
+
+  connection: Env.get('DB_CONNECTION'),
+
+  connections: {
+    pg: {
+      client: 'pg',
+      connection: {
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
+
+  }
+}
+
+export default databaseConfig
+// TODO I need to understand connection with db and migrations with adonis
