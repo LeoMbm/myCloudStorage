98d9a4b804ff0217a17f5ae7c0c4b9480450e2db
LeoMbm <leonidas.j.mbm@gmail.com>
2022-08-28 23:32:06 +0200

    update

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -7,7 +7,6 @@ import User from 'App/Models/User'
 export default class UsersController {
   public async register({ request }: HttpContextContract){
     const payload = await request.validate(StoreUserValidator);
-    // const body = request.body();
     const user = await User.create(payload)
     return {message: user}
   }
